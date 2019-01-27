ccbe72ae87f7f113b2d9eb20edbac7e1e8083a8a
github username <github email address>
2019-01-27 13:27:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+// function upperCaseCustomerName(name){
+//   return name.toUpperCase();
+// }
+// customerName = upperCaseCustomerName(customerName);
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  var bestCustomer = "not bob";
+}
